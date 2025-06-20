f4d4c57edd1ec9e58c1988d4142095e121be8e90
Guillaume <guillaume.zahnd@ithera-medical.com>
2025-06-20 11:27:38 +0200

    Add demo and wrapper

---
+++ README.md
@@ -0,0 +1,15 @@
+# Question answering
+
+## `question_answering_bert_finetuned_squad_demo.ipynb`
+
+**context:**
+
+> Normally the calendar advances 1 day, but you may choose to advance it 2 days instead. This privilege is subject to limitations: You cannot use this privilege if your board is darker side up. When you use this privilege, you must flip your board over to the darker side to show that you have used it. (If you reach the top step of a temple, you can flip it back. flip it back, as explained in the next section, under 'Temples'.) You may not use this privilege to push off a worker that would not normally be pushed off a gear. In other words, you may not use this privilege if there is a worker on action space 6 of one of the four smallest gears or action space 9 of the Chichen Itza gear. Even if you cannot use the time-speeding privilege, you still get all the other benefits of choosing the Starting Player Space. It is not possible to use this privilege to avoid Food Days. If the next day on the Tzolk'in gear is a Food Day, you may still advance 2 days, but the new round will be the Food Day that was skipped over.
+
+**question:**
+
+> How can a player flip their board back?
+
+**answer:**
+
+> if you reach the top step of a temple
---
+++ question_answering_bert_finetuned_squad_wrapper.py
@@ -0,0 +1,41 @@
+import torch
+import numpy as np
+from utils_nlp import get_model_and_tokenizer
+
+
+def question_answering_bert_finetuned_squad_wrapper(context: str, question: str) -> str:
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    model_checkpoint = 'bert-large-uncased-whole-word-masking-finetuned-squad'
+    model, tokenizer = get_model_and_tokenizer(model_checkpoint=model_checkpoint, device=device)
+    
+    batch_encoding_tmp = tokenizer.encode_plus(text=question, text_pair=context)
+    input_ids = batch_encoding_tmp['input_ids']
+    
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    
+    segment_ids = get_segment_ids_from_input_ids(input_ids=input_ids, tokenizer=tokenizer)
+    
+    question_answering_model_output_tmp = model(
+        torch.tensor([input_ids]).to(device),
+        token_type_ids=torch.tensor([segment_ids]).to(device))    
+        
+    start_logits = question_answering_model_output_tmp['start_logits'][0].detach().cpu().numpy()
+    end_logits = question_answering_model_output_tmp['end_logits'][0].detach().cpu().numpy()        
+        
+    answer_start = np.argmax(start_logits)
+    answer_end = np.argmax(end_logits)
+    answer = ' '.join(tokens[answer_start:answer_end+1])
+    
+    return answer
+
+    
+def get_segment_ids_from_input_ids(input_ids, tokenizer):
+    # input:  '[CLS]', 'x', 'x', '[SEP]', 'x', 'x', 'x', 'x', 'x', '[SEP]'
+    # output: [0,       0,   0,   0,       1,   1,   1,   1,   1,   1] 
+    separator_index = input_ids.index(tokenizer.sep_token_id)
+    length_of_first_segment = separator_index + 1
+    length_of_second_segment = len(input_ids) - length_of_first_segment
+    segment_ids = [0]*length_of_first_segment + [1]*length_of_second_segment    
+    return segment_ids
